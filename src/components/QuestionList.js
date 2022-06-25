14267397a5b8d15fd1b7602e93b07a3123b725e5
Marcos Valderrabano <ing.marco.valu@gmail.com>
2022-06-24 23:03:41 -0700

    Completed lab

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,23 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, deleteQuestion }) {
+  function handleDeleteQuestion(questionDeleted) {
+    deleteQuestion(questionDeleted);
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        { questions.map((question) => (
+          <QuestionItem 
+            key={ question.id } 
+            question={ question }
+            onDeleteQuestion={ handleDeleteQuestion }
+          />
+        ))}
+      </ul>
     </section>
   );
 }
